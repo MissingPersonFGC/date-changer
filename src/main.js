3bf2e2d1858a2fa839f9d67d40d2ecde96053d78
Corey Lanier <iam@coreylanier.com>
2020-03-14 17:08:18 -0400

    added csv reader

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-
+import { VueCsvImport } from "vue-csv-import";
 import vSelect from "vue-select";
 import { Datetime } from "vue-datetime";
 
 Vue.component("v-select", vSelect);
 
 Vue.component("datetime", Datetime);
+
+Vue.component("vue-csv-import", VueCsvImport);
 
 Vue.config.productionTip = false;
 
