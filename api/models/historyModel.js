8218e3aeedd06f25f6abd1e6de2f379152de8b4e
Corey Lanier <iam@coreylanier.com>
2020-03-19 19:44:49 -0400

    Created mongoose model for tracking update history

---
+++ api/models/historyModel.js
@@ -0,0 +1,61 @@
+"use strict";
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const { model: User } = require("./userModel");
+const { model: Teacher } = require("./teacherModel");
+
+const historySchema = new Schema({
+  date: {
+    type: Date,
+    required: true,
+    default: Date.now()
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: User,
+    required: true
+  },
+  teacher: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: Teacher,
+    required: true
+  },
+  course: {
+    type: String,
+    required: true
+  },
+  courseName: {
+    type: String,
+    required: true
+  },
+  assignment: {
+    type: String,
+    required: true
+  },
+  assignmentName: {
+    type: String,
+    required: true
+  },
+  editType: {
+    type: String,
+    enum: ["Dates", "Extension"],
+    required: true
+  },
+  unlockDate: {
+    type: Date
+  },
+  lockDate: {
+    type: Date
+  },
+  dueDate: {
+    type: Date
+  },
+  extensionDate: {
+    type: Date
+  },
+  students: {
+    type: [String]
+  }
+});
+
+exports.model = mongoose.model("History", historySchema);
