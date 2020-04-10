361624cddbdef048eaa7e233a8edb6ce9093a735
Corey Lanier <iam@coreylanier.com>
2020-04-10 19:34:33 -0400

    created function to parse cookie

--- _gradefixer.js
+++ _gradefixer.js
@@ -13,4 +13,11 @@
   const contextType = courseMatch[2];
   const contextId =
     typeof courseMatch[3] === "undefined" ? false : courseMatch[3];
+
+  function getCookie(name) {
+    return document.cookie.split(";").reduce(function(a, c) {
+      const d = c.trim().split("=", 2);
+      return d[0] === name ? decodeURIComponent(d[1]) : a;
+    }, "");
+  }
 });
