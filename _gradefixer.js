7e2fee48a5af72a1341bea1da6f8323a44c60361
Corey <iam@coreylanier.com>
2020-04-11 18:42:14 -0400

    created comment for permanent zero dates

--- _gradefixer.js
+++ _gradefixer.js
@@ -1,6 +1,17 @@
 (function() {
   "use strict";
-  const config = {};
+
+  // first two should be true whenever you run this the first time on a course's gradebook. You should run it a second time with the first two false and the last one true to finalize the fixes.
+  const config = {
+    comment: true, // add comment showing permanent zero dates, do at the same time as the fix
+    zero: true, // add temporary zero in order to fix the display of assignments in student gradebook
+    reset: false // removes zeroes, but leaves comments. Make this true, and the others false on second pass.
+  };
+
+  const comment = date => {
+    return `A score of zero will become permanent on ${date}. Please submit work before this date.`;
+  };
+
   const courseRegex = new RegExp(
     "^/courses/(\\d+)/(assignments|quizzes|discussion_topics)(?:/(\\d+))?"
   );
