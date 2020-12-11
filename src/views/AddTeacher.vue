d272eedfbe07c81c3d985f44b26d7679a0e74c7b
Corey <iam@coreylanier.com>
2020-12-11 18:23:43 -0500

    wrote out base component to add teacher

--- src/views/AddTeacher.vue
+++ src/views/AddTeacher.vue
@@ -1,11 +1,43 @@
 <template>
-	<div>
+	<div class="add-teacher">
 		<h1>Add a Teacher</h1>
+		<form @submit.prevent="addTeacher">
+			<p>First Name:</p>
+			<input type="text" v-model="firstName" />
+			<p>Last Name</p>
+			<input type="text" v-model="lastName" />
+			<p>Designation:</p>
+			<input type="text" v-model="designation" />
+			<p>API Key:</p>
+			<input type="text" v-model="apiKey" />
+			<button type="submit">Add Teacher</button>
+		</form>
 	</div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-	name: "add-teacher"
+	name: "add-teacher",
+	data() {
+		return {
+			firstName: "",
+			lastName: "",
+			designation: "",
+			apiKey: "",
+			success: false
+		};
+	},
+	addTeacher: async function() {
+		const teacher = this.$data;
+		const res = await axios.post("/api/teachers", {
+			teacher
+		});
+		this.data.success = true;
+		this.data.firstName = "";
+		this.data.lastName = "";
+		this.data.designation = "";
+		this.data.apiKey = "";
+	}
 };
 </script>
